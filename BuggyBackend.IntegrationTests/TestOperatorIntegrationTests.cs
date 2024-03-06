0f70b9a8a24a31d81ecb94b9fc977b4ddc8ef806
Dennis Rasmussen <dennis@deras.dk>
2024-03-06 23:52:48 +0100

    This backend is ready to be shipped

--- BuggyBackend.IntegrationTests/TestOperatorIntegrationTests.cs
+++ BuggyBackend.IntegrationTests/TestOperatorIntegrationTests.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Net;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 
@@ -30,5 +31,32 @@ public class BasicTests
         // Assert
         response.EnsureSuccessStatusCode(); // Status Code 200-299
         Assert.True(stopwatch.ElapsedMilliseconds <= 1000, "Response time is more than 1 second");
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(20)]
+    [InlineData(30)]
+    [InlineData(31, true)]
+    [InlineData(32, true)]
+    [InlineData(132, true)]
+    public async Task TestResultsEndpointPageSizeParameter(int pageSize, bool throws = false)
+    {
+        string url = "/testresults";
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync($"{url}?page=1&pagesize={pageSize}");
+
+        // Assert
+        if (!throws)
+        {
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+        }
+        else
+        {
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
     }
 }
